6a2e04fbb5249d484e2650fbac23d00346e21404
Ptrnd <putranda.bagus@gmail.com>
2020-12-12 18:40:27 +0700

    menggunakan put daripada patch

--- routes/api.php
+++ routes/api.php
@@ -24,22 +24,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('user', [UserController::class, 'get_all_user']);
 Route::get('user/{id}', [UserController::class, 'getUserById']);
 Route::post('user/tambah', [UserController::class, 'insert_user']);
-// Route::put('user/update/{id}', [UserController::class, 'update_user']);
-Route::patch('user/update/{id}', [UserController::class, 'update_user']);
+Route::put('user/update/{id}', [UserController::class, 'update_user']);
+// Route::patch('user/update/{id}', [UserController::class, 'update_user']);
 Route::delete('user/delete/{id}', [UserController::class, 'delete_user']);
 
 Route::get('booking', [BookingController::class, 'get_all_booking']);
 Route::get('booking/{id}', [BookingController::class, 'getBookingById']);
 Route::post('booking/tambah', [BookingController::class, 'insert_booking']);
-// Route::put('booking/update/{id}', [BookingController::class, 'update_booking']);
-Route::patch('booking/update/{id}', [BookingController::class, 'update_booking']);
+Route::put('booking/update/{id}', [BookingController::class, 'update_booking']);
+// Route::patch('booking/update/{id}', [BookingController::class, 'update_booking']);
 Route::delete('booking/delete/{id}', [BookingController::class, 'delete_booking']);
 
 Route::get('dokter', [DokterController::class, 'get_all_dokter']);
 Route::get('dokter/{id}', [DokterController::class, 'getDokterById']);
 Route::post('dokter/tambah', [DokterController::class, 'insert_dokter']);
-// Route::put('dokter/update/{id}', [DokterController::class, 'update_dokter']);
-Route::patch('dokter/update/{id}', [DokterController::class, 'update_dokter']);
+Route::put('dokter/update/{id}', [DokterController::class, 'update_dokter']);
+// Route::patch('dokter/update/{id}', [DokterController::class, 'update_dokter']);
 Route::delete('dokter/delete/{id}', [DokterController::class, 'delete_dokter']);
 
 
